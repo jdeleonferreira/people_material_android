dd22d78a44b8d32e25e34656d4e92da5c03603bc
jdeleon <jdeleonferreira@gmail.com>
2020-11-20 21:26:12 -0500

    Create person interface finished

---
+++ app/src/main/java/com/example/peoplematerial/CreatePerson.java
@@ -0,0 +1,14 @@
+package com.example.peoplematerial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreatePerson extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_person);
+    }
+}
